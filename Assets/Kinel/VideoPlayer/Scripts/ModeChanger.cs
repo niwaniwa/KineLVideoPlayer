6b64f42476072b720411f39494d06e360deb26b0
niwaniwa <konya.kokekokko@gmail.com>
2021-02-17 21:53:09 +0900

    モード変更の挙動を変更 - より柔軟性を持たせるように変更

--- Assets/Kinel/VideoPlayer/Scripts/ModeChanger.cs
+++ Assets/Kinel/VideoPlayer/Scripts/ModeChanger.cs
@@ -3,6 +3,9 @@ using Kinel.VideoPlayer.Scripts;
 using UdonSharp;
 using UnityEngine;
 using UnityEngine.UI;
+using VRC.SDK3.Video.Components;
+using VRC.SDK3.Video.Components.AVPro;
+using VRC.SDK3.Video.Components.Base;
 using VRC.SDKBase;
 using VRC.Udon;
 using VRC.Udon.Common.Interfaces;
@@ -10,57 +13,84 @@ using VRC.Udon.Common.Interfaces;
 public class ModeChanger : UdonSharpBehaviour
 {
 
-    public KinelVideoScript videoPlayer;
-    public VideoTimeSliderController sliderController;
-    public Toggle video, stream;
-    public GameObject timeText;
+    [SerializeField] private VRCUnityVideoPlayer video;
+    [SerializeField] private VRCAVProVideoPlayer stream;
+    [SerializeField] private KinelVideoScript videoPlayer;
+    [SerializeField] private VideoTimeSliderController sliderController;
+    //public VideoPlayerTimeTextUpdater textUpdater;
+    [SerializeField] private TogglePlayButton togglePlayButton;
+    [SerializeField] private Animator animator;
+    [SerializeField] private GameObject textUpdater;
 
-    public void ChangeStreamModeForVideoButton()
+    [SerializeField] private GameObject videoScreen;
+    [SerializeField] private GameObject streamScreen;
+    
+    private const int VIDEO_MODE = 0;
+    private const int STREAM_MODE = 1;
+    
+
+    public void ChangeMode(int playMode)
     {
-        if (video.isOn)
+        if (videoPlayer.GetVideoPlayer() != null)
+            videoPlayer.ResetLocal();
+
+        if (playMode == VIDEO_MODE)
         {
-            return;
+            videoPlayer.SetVideoInstance(video);
+            textUpdater.SetActive(true);
+            videoScreen.SetActive(true);
+            streamScreen.SetActive(false);
+            sliderController.UnFreeze();
+            togglePlayButton.gameObject.SetActive(true);
+            animator.SetInteger("PlayMode", VIDEO_MODE);
         }
 
-        ChangeStreamMode();
+        if (playMode == STREAM_MODE)
+        {
+            videoPlayer.SetVideoInstance(stream);
+            textUpdater.SetActive(false);
+            videoScreen.SetActive(false);
+            streamScreen.SetActive(true);
+            sliderController.Freeze();
+            togglePlayButton.gameObject.SetActive(false);
+            animator.SetInteger("PlayMode", STREAM_MODE);
+        }
+        
+        videoPlayer.ResetLocal();
+        
+    }
+
+    public void ChangeModeForSlider()
+    {
+        if (videoPlayer.masterOnly)
+            return;
+        
+        if (videoPlayer.GetPlayMode() == VIDEO_MODE)
+            ChangeStreamModeForStreamButton();
+        else
+            ChangeStreamModeForVideoButton();
+        
+    }
+    
+    public void ChangeStreamModeForVideoButton()
+    {
+        if (videoPlayer.GetPlayMode() == VIDEO_MODE)
+            return;
+        
+        Debug.Log($"[KineL] Mode changed from button. to [video]");
+        ChangeMode(VIDEO_MODE);
+        videoPlayer.GlobalChangeMode(VIDEO_MODE);
     }
     
     public void ChangeStreamModeForStreamButton()
     {
-        if (stream.isOn)
-        {
+        if (videoPlayer.GetPlayMode() == STREAM_MODE)
             return;
-        }
-
-        ChangeStreamMode();
+        
+        Debug.Log($"[KineL] Mode changed from button. to [stream]");
+        ChangeMode(STREAM_MODE);
+        videoPlayer.GlobalChangeMode(STREAM_MODE);
     }
 
-    private void ChangeStreamMode()
-    {
-        videoPlayer.Reset();
-        Networking.SetOwner(Networking.LocalPlayer, this.gameObject);
-        SendCustomNetworkEvent(NetworkEventTarget.All, "ChangeStreamModeGlobal");
-    }
 
-    public void ChangeStreamModeGlobal()
-    {
-        videoPlayer.SetStreamModeFlagLocal(!videoPlayer.IsStreamLocal());
-        videoPlayer.SetVideoInstance(videoPlayer.IsStreamLocal());
-        SetToggleActive();
-        if (Networking.IsOwner(Networking.LocalPlayer, this.gameObject))
-        {
-            videoPlayer.SetStreamModeFlagGlobal(videoPlayer.IsStreamLocal());
-            sliderController.Freeze();
-            return;
-        }
-        sliderController.Freeze();
-    }
-
-    public void SetToggleActive()
-    {
-        stream.isOn = videoPlayer.IsStreamLocal();
-        video.isOn = !videoPlayer.IsStreamLocal();
-        timeText.SetActive(!timeText.activeSelf);
-    }
-    
 }
