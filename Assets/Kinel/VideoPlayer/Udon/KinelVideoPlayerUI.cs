1e51c5456e612a9ce9a784a9a250a6f7b6bb5b26
niwaniwa <konya.kokekokko@gmail.com>
2022-02-24 23:54:38 +0900

    UI用のUdonスクリプトを追加

---
+++ Assets/Kinel/VideoPlayer/Udon/KinelVideoPlayerUI.cs
@@ -0,0 +1,51 @@
+using System;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDK3.Video.Components.Base;
+
+namespace Kinel.VideoPlayer.Udon
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
+    public class KinelVideoPlayerUI : UdonSharpBehaviour
+    {
+        
+        [SerializeField] public KinelVideoPlayer videoPlayer;
+        
+        [SerializeField] public bool isAutoFill = false;
+        private bool _isUIActive = true;
+
+        public void Start()
+        {
+            if (!videoPlayer)
+            {
+                _isUIActive = false;
+            }
+        }
+
+        public KinelVideoPlayer GetVideoPlayer()
+        {
+            return videoPlayer;
+        }
+
+        public BaseVRCVideoPlayer GetSystem()
+        {
+            return videoPlayer.GetVideoPlayerController().GetCurrentVideoPlayer();
+        }
+
+        public void RegisterListener(UdonSharpBehaviour udon)
+        {
+            videoPlayer.RegisterListener(udon);
+        }
+
+        public void UnregisterListener(UdonSharpBehaviour udon)
+        {
+            videoPlayer.UnregisterListener(udon);
+        }
+        
+        public bool IsUIAcitve()
+        {
+            return _isUIActive;
+        }
+
+    }
+}
