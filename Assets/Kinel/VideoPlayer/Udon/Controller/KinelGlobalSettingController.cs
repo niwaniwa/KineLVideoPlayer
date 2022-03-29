13dce84d4d4d5ce24e1030cd23c3a2e90590ce3b
niwaniwa <konya.kokekokko@gmail.com>
2022-03-29 23:45:30 +0900

    不可の関係でanimationの使用をやめました

--- Assets/Kinel/VideoPlayer/Udon/Controller/KinelGlobalSettingController.cs
+++ Assets/Kinel/VideoPlayer/Udon/Controller/KinelGlobalSettingController.cs
@@ -1,5 +1,6 @@
 ﻿using UdonSharp;
 using UnityEngine;
+using UnityEngine.UI;
 using VRC.SDKBase;
 
 namespace Kinel.VideoPlayer.Udon.Controller
@@ -10,9 +11,11 @@ namespace Kinel.VideoPlayer.Udon.Controller
     {
 
         [SerializeField] private KinelVideoPlayerUI videoPlayerUI;
-        [SerializeField] private Animator uiAnimator;
+        // [SerializeField] private Animator uiAnimator;
         [SerializeField] private GameObject loopActive, loopDisable;
         [SerializeField] private GameObject[] lockToggleObjects;
+        [SerializeField] private Slider lockSlider, modeSlider;
+        [SerializeField] private Image lockBackground, modeBackground;
         
         public const int VIDEO_MODE = 0;
         public const int STREAM_MODE = 1;
@@ -25,31 +28,33 @@ namespace Kinel.VideoPlayer.Udon.Controller
 
         public void OnResetClick()
         {
+            TakeOwnership();
             videoPlayerUI.GetVideoPlayer().ResetGlobal();
         }
 
         public void OnReloadClick()
         {
+            TakeOwnership();
             videoPlayerUI.GetVideoPlayer().ReloadGlobal();
         }
 
         public void OnLoopClick()
         {
-            if (!Networking.IsOwner(Networking.LocalPlayer, videoPlayerUI.GetVideoPlayer().gameObject))
-                return;
-            
+            TakeOwnership();
             videoPlayerUI.GetVideoPlayer().Loop = !videoPlayerUI.GetVideoPlayer().Loop;
+            videoPlayerUI.GetVideoPlayer().RequestSerialization();
             ToggleLoopIcon();
             
         }
 
         public void OnLockChangeToggle()
         {
+#if !UNITY_EDITOR
             if (!Networking.LocalPlayer.isMaster)
                 return;
-
+#endif
+            TakeOwnership();
             var videoPlayer = videoPlayerUI.GetVideoPlayer();
-            videoPlayer.TakeOwnership();
             videoPlayer.IsLock = !videoPlayer.IsLock;
             videoPlayer.RequestSerialization();
 
@@ -57,6 +62,7 @@ namespace Kinel.VideoPlayer.Udon.Controller
 
         public void OnModeChange()
         {
+            TakeOwnership();
             videoPlayerUI.GetVideoPlayer().TakeOwnership();
             switch (videoPlayerUI.GetVideoPlayer().GetCurrentVideoMode())
             {
@@ -78,7 +84,9 @@ namespace Kinel.VideoPlayer.Udon.Controller
 
         public void OnKinelVideoPlayerLocked()
         {
-            uiAnimator.SetBool("MasterLock", true);
+            // uiAnimator.SetBool("MasterLock", true);
+            lockSlider.value = 0.8f;
+            lockBackground.color = new Color(1f, 0.5f, 0.5f, 1.0f);
             
             if (Networking.LocalPlayer.isMaster)
                 return;
@@ -92,8 +100,9 @@ namespace Kinel.VideoPlayer.Udon.Controller
 
         public void OnKinelVideoPlayerUnlocked()
         {
-            uiAnimator.SetBool("MasterLock", false);
-            
+            // uiAnimator.SetBool("MasterLock", false);
+            lockSlider.value = 0.15f;
+            lockBackground.color = new Color(1f, 1f, 1f, 1f);
             if (Networking.LocalPlayer.isMaster)
                 return;
             
@@ -105,13 +114,44 @@ namespace Kinel.VideoPlayer.Udon.Controller
 
         public void OnKinelVideoModeChange()
         {
-            uiAnimator.SetInteger("Mode", videoPlayerUI.GetVideoPlayer().GetCurrentVideoMode());
+            // uiAnimator.SetInteger("Mode", videoPlayerUI.GetVideoPlayer().GetCurrentVideoMode());
+
+            if (videoPlayerUI.GetVideoPlayer().GetCurrentVideoMode() == VIDEO_MODE)
+            {
+                modeSlider.value = 0.15f;
+                modeBackground.color = new Color(1f, 1f, 1f, 1.0f);
+            }
+            else
+            {
+                modeSlider.value = 0.85f;
+                modeBackground.color = new Color(1f, 0.5f, 0.5f, 1.0f);
+            }
         }
 
         private void ToggleLoopIcon()
         {
             loopActive.SetActive(videoPlayerUI.GetVideoPlayer().Loop);
             loopDisable.SetActive(!videoPlayerUI.GetVideoPlayer().Loop);
+        }
+
+        private void TakeOwnership()
+        {
+
+            if (Networking.IsOwner(Networking.LocalPlayer, gameObject))
+            {
+                if (!Networking.IsOwner(Networking.LocalPlayer, videoPlayerUI.GetVideoPlayer().gameObject))
+                {
+                    videoPlayerUI.GetVideoPlayer().TakeOwnership();
+                    return;
+                }
+
+                return;
+            }
+            
+            Networking.SetOwner(Networking.LocalPlayer, gameObject);
+            videoPlayerUI.GetVideoPlayer().TakeOwnership();
+            
+            
         }
         
         
