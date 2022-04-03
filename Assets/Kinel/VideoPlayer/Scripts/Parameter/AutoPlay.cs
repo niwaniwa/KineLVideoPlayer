d72e672382ba2124cba99dfc90c734cc7e18162e
niwaniwa <konya.kokekokko@gmail.com>
2022-04-03 21:39:35 +0900

    VideoPlayer用のパラメータを追加

---
+++ Assets/Kinel/VideoPlayer/Scripts/Parameter/AutoPlay.cs
@@ -0,0 +1,8 @@
+namespace Kinel.VideoPlayer.Scripts
+{
+    public enum AutoPlay
+    {
+        None = 0,
+        PlayerWhenJoin = 1
+    }
+}
---
+++ Assets/Kinel/VideoPlayer/Scripts/Parameter/Loop.cs
@@ -0,0 +1,9 @@
+namespace Kinel.VideoPlayer.Scripts
+{
+    public enum Loop
+    {
+        None = 0,
+        Repeat = 1,
+        RepeatAll = 2
+    }
+}
