4e76b315393e6733efe4786b7fe71f66f8020965
niwaniwa <konya.kokekokko@gmail.com>
2022-04-03 21:07:43 +0900

    プレイリスト関連のEditor拡張の追加

---
+++ Assets/Kinel/VideoPlayer/Editor/Playlist/KinelQueuePlaylistEditor.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections.Generic;
+using Kinel.VideoPlayer.Editor.Internal;
+using Kinel.VideoPlayer.Scripts;
+using Kinel.VideoPlayer.Scripts.Parameter;
+using Kinel.VideoPlayer.Udon;
+using Kinel.VideoPlayer.Udon.Playlist;
+using UdonSharpEditor;
+using UnityEditor;
+using UnityEngine;
+using VRC.SDKBase;
+
+namespace Kinel.VideoPlayer.Editor
+{
+    [CustomEditor(typeof(KinelQueuePlaylistScript))]
+    public class KinelQueuePlaylistEditor : KinelEditorBase
+    {
+
+        private KinelQueuePlaylistScript _playlist;
+        private SerializedProperty _kinelVideoPlayer, _isAutoFill;
+        
+        public void OnEnable()
+        {
+            _playlist = target as KinelQueuePlaylistScript;
+            _kinelVideoPlayer = serializedObject.FindProperty(nameof(KinelQueuePlaylistScript.videoPlayer));
+            _isAutoFill = serializedObject.FindProperty(nameof(KinelQueuePlaylistScript.isAutoFill));
+
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            
+            serializedObject.Update();
+            
+            
+            // playlist header
+            EditorGUILayout.BeginVertical(GUI.skin.box);
+            {
+                EditorGUILayout.LabelField("KineL Queue Playlist");
+            }
+            EditorGUILayout.EndVertical();
+
+            EditorGUILayout.Space();
+            
+            // playlist main
+            EditorGUILayout.BeginVertical(GUI.skin.box);
+            {
+                EditorGUILayout.LabelField("基本設定");
+                EditorGUI.indentLevel++;
+                EditorGUILayout.Space();
+                EditorGUILayout.PropertyField(_kinelVideoPlayer);
+                AutoFillProperties();
+                if (_isAutoFill.enumValueIndex == (int) FillResult.Success)
+                {
+                    EditorGUILayout.HelpBox("自動的に設定されました。", MessageType.Info);
+                } 
+                else if (_isAutoFill.enumValueIndex == (int) FillResult.AlreadyExistence)
+                {
+                    EditorGUILayout.HelpBox("設定されました。", MessageType.Info);
+                }
+                EditorGUILayout.Space();
+                EditorGUI.indentLevel--;
+            }
+            EditorGUILayout.EndVertical();
+            
+            if (serializedObject.ApplyModifiedProperties())
+            {
+                ApplyPlaylistProperties();
+                ApplyUdonProperties();
+            }
+        }
+
+        public override void ApplyUdonProperties()
+        {
+            var playlist = _playlist.GetUdonSharpComponentInChildren<KinelQueuePlaylist>();
+
+            if (playlist == null)
+            {
+                Debug.LogError($"{DEBUG_ERROR_PREFIX} playlist null");
+                return;
+            }
+            Undo.RecordObject(playlist, "Instance attached");
+            playlist.UpdateProxy();
+            playlist.SetProgramVariable("videoPlayer", _kinelVideoPlayer.objectReferenceValue);
+            playlist.ApplyProxyModifications();
+            UdonSharpEditorUtility.CopyProxyToUdon(playlist);
+            KinelEditorUtilities.UpdateKinelVideoUIComponents(playlist);
+            EditorUtility.SetDirty(playlist);
+            // AssetDatabase.SaveAssets();
+            
+            
+
+        }
+
+        private void ApplyPlaylistProperties()
+        {
+            
+        }
+
+        private void AutoFillProperties()
+        {
+            
+            _isAutoFill.enumValueIndex = (int) KinelEditorUtilities.FillUdonSharpInstance<KinelVideoPlayer>(_kinelVideoPlayer, _playlist.gameObject, false);
+        }
+        
+        
+
+        
+    }
+}
