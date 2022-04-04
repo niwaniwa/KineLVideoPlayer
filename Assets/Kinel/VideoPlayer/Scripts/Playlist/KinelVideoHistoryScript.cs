c175e745cdec16a8bd2799da71447f0615f6be6e
niwaniwa <konya.kokekokko@gmail.com>
2022-04-04 19:09:53 +0900

    Playlisy用Scriptの追加

---
+++ Assets/Kinel/VideoPlayer/Scripts/Playlist/KinelVideoHistoryScript.cs
@@ -0,0 +1,14 @@
+using Kinel.VideoPlayer.Scripts.Parameter;
+using Kinel.VideoPlayer.Udon;
+using UnityEngine;
+
+namespace Kinel.VideoPlayer.Scripts
+{
+    public class KinelVideoHistoryScript : MonoBehaviour
+    {
+        public KinelVideoPlayer videoPlayer;
+        
+        public FillResult isAutoFill;
+        public bool saveErrorUrl;
+    }
+}
