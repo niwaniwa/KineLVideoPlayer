82fccf2613453b082d51a870f21381e8040a0c76
niwaniwa <konya.kokekokko@gmail.com>
2022-04-06 17:58:31 +0900

    Editor用のScriptを追加

--- Assets/Kinel/VideoPlayer/Scripts/KinelVideoPlayerScript.cs
+++ Assets/Kinel/VideoPlayer/Scripts/KinelVideoPlayerScript.cs
@@ -1,7 +1,18 @@
+using System;
+using Kinel.VideoPlayer.Udon;
+using UnityEditor;
+using VRC.SDKBase;
+
 namespace Kinel.VideoPlayer.Scripts
 {
+    
     public class KinelVideoPlayerScript : KinelScriptBase
     {
-        
+        public KinelVideoPlayer videoPlayer;
+        public bool loop, isAutoFill, enableErrorRetry, enableDefaultUrl;
+        public int retryLimit;
+        public float deleyLimit;
+        public string defaultUrl;
+        public int defaultUrlMode;
     }
 }
