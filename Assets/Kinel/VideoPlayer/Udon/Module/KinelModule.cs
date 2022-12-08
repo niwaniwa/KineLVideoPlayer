6650528861814ab03e7dad216325432d7805029c
koke <konya.kokekokko@gmail.com>
2022-12-09 00:11:08 +0900

    Add base module

---
+++ Assets/Kinel/VideoPlayer/Udon/Module/KinelModule.cs
@@ -0,0 +1,22 @@
+﻿using UdonSharp;
+
+namespace Kinel.VideoPlayer.Udon.Module
+{
+    public class KinelModule : UdonSharpBehaviour
+    {
+
+        public virtual void OnKinelUrlUpdate()
+        {
+            
+        }
+        public virtual void OnKinelVideoReady(){}
+        public virtual void OnKinelVideoStart(){}
+        public virtual void OnKinelVideoEnd(){}
+        public virtual void OnKinelVideoLoop(){}
+        public virtual void OnKinelVideoError(){}
+        public virtual void OnKinelVideoRetryError(){}
+        public virtual void OnKinelChangeVideoTime(){}
+        public virtual void OnKinelVideoModeChange(){}
+
+    }
+}
